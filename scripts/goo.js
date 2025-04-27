0f4d3574262cf4bf8cc45fcf1426068f6309a7e8
Greenfenix414 <greenfenix414@gmail.com>
2025-04-27 17:02:50 -0500

    compaction

---
+++ scripts/goo.js
@@ -0,0 +1,5 @@
+    // Goo Flood!
+    flood.texture = "/site/art/assets/goop.png";
+    flood.textureWidth = 256; // Resize to match your image file
+    flood.textureHeight = 256;
+    // Goo Messages

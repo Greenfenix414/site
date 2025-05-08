f9c3d3ebcccf6ed77923e3d3cf158f843c9c715f
Greenfenix414 <greenfenix414@gmail.com>
2025-05-08 17:01:41 -0500

    canvas

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+function setup(e) {
+  createCanvas(windowWidth, windowHeight, mainCanvas);
+  ralph = new blob("chartreuse")
+}
+
+function draw() {
+  clear()
+  ralph.drawBlob()
+  strokeWeight(2);
+
+}
